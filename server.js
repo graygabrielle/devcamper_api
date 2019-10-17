8adcebfdb78e7f9aeaff7d091fe8601aa7019e08
graygabrielle <gray.atkinson.adams@gmail.com>
2019-10-17 15:01:53 +0200

    created CRUD functionality for bootcamps

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
 const connectDB = require("./config/db");
 
 //load env vars
@@ -14,6 +15,9 @@ const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
+//body parser middleware
+app.use(express.json());
+
 //dev logging middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -26,12 +30,14 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 //handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red.underline);
   //close server and exit process
   server.close(() => {
     process.exit(1);
