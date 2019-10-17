a12e41e2333fd56c9f7805bb4badeabcec819736
graygabrielle <gray.atkinson.adams@gmail.com>
2019-10-17 11:06:01 +0200

    installed morgan as dev logging middleware

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.use(morgan);
+//dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 //mount routers
 app.use("/api/v1/bootcamps", bootcamps);
