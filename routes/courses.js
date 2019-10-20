95bdf9f8197a638b2341ac05be65f1dad1f8350d
graygabrielle <gray.atkinson.adams@gmail.com>
2019-10-20 20:10:39 +0200

    added routes and controllers for courses

--- routes/courses.js
+++ routes/courses.js
@@ -3,7 +3,8 @@ const {
   getCourses,
   getCourse,
   addCourse,
-  updateCourse
+  updateCourse,
+  deleteCourse
 } = require("../controllers/courses");
 const router = express.Router({ mergeParams: true });
 
@@ -15,6 +16,7 @@ router
 router
   .route("/:id")
   .get(getCourse)
-  .put(updateCourse);
+  .put(updateCourse)
+  .delete(deleteCourse);
 
 module.exports = router;
